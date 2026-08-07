79ad36eb3e6bb4006c66392ccdf513ccf97841a8
TheItcor <itcorsmail@gmail.com>
2026-08-07 22:58:06 +0900

    feat(gradle): update gradle.settings.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,12 @@
 plugins {
     java
     application
-    id("org.javamodularity.moduleplugin") version "1.8.15"
     id("org.openjfx.javafxplugin") version "0.0.13"
-    id("org.beryx.jlink") version "2.25.0"
+    id("org.beryx.jlink") version "3.1.3"
 }
 
 group = "itcor"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -25,7 +24,7 @@ tasks.withType<JavaCompile> {
 
 application {
     mainModule.set("itcor.pomeedoro")
-    mainClass.set("itcor.pomeedoro.HelloApplication")
+    mainClass.set("itcor.pomeedoro.Main")
 }
 
 javafx {
@@ -33,19 +32,23 @@ javafx {
     modules = listOf("javafx.controls", "javafx.fxml", "javafx.media")
 }
 
-dependencies {
-        compileOnly("org.projectlombok:lombok:1.18.46")
-        annotationProcessor("org.projectlombok:lombok:1.18.46")
+jlink {
+    options.set(listOf("--strip-debug", "--compress", "zip-6", "--no-header-files", "--no-man-pages"))
+    launcher { name = "pomeedoro" }
+
+    jpackage {
+        imageName = "Pomeedoro"
+        appVersion = "1.0"
+        vendor = "Itcor"
+
+        installerType = when {
+            org.gradle.internal.os.OperatingSystem.current().isWindows -> "exe"
+            org.gradle.internal.os.OperatingSystem.current().isMacOsX -> "dmg"
+            else -> (project.findProperty("pkg") as? String) ?: "deb"
+        }
+    }
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
-}
-
-jlink {
-    imageZip.set(layout.buildDirectory.file("/distributions/app-${javafx.platform.classifier}.zip"))
-    options.set(listOf("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages"))
-    launcher {
-        name = "app"
-    }
 }
